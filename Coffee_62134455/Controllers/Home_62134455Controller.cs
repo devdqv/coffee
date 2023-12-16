d06200abb5516545ed457b73fc8d24e5a24fb8e7
devdqv <vectovinh@gmail.com>
2023-12-16 23:43:14 +0700

    Load Sản phẩm ra trang chủ

--- Coffee_62134455/Controllers/Home_62134455Controller.cs
+++ Coffee_62134455/Controllers/Home_62134455Controller.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Coffee_62134455.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +13,30 @@ namespace Coffee_62134455.Controllers
     {
         public ActionResult Index()
         {
+            using (var db = new DbContext_62134455())
+            {
+                //Lấy ra 4 sản phẩm bán chạy nhất
+                ViewBag.SPBanChayNhat = db.SanPhams_62134455.Take(4).Include(x => x.DanhMucs_62134455).ToList();
+                var danhMucs = db.DanhMucs_62134455.OrderBy(x=>x.TenDanhMuc).ToList();
+                ViewBag.DanhMucSP = danhMucs;
+                ViewBag.FirstDanhMuc = danhMucs.FirstOrDefault();
+            }    
             return View();
+        }
+
+        /// <summary>
+        /// Lấy Sản phẩm theo từng danh mục khi khách hàng kích ở trang chủ, lấy 4 cái 1 lần
+        /// </summary>
+        /// <param name="id_danhmuc"></param>
+        /// <returns></returns>
+        public ActionResult SanPhamTheoDanhMuc(int? id_danhmuc)
+        {
+            List<SanPhams_62134455> listSanPham;
+            using (var db = new DbContext_62134455())
+            {
+                listSanPham = db.SanPhams_62134455.Take(4).Where(x => x.id_danhmuc == id_danhmuc).ToList();
+            }
+            return View(listSanPham);
         }
 
         public ActionResult ThucDon()
